fe7105806470ec5842d01a5bbe7d55a9e4992f5b
David Sherret <dsherret@gmail.com>
2026-03-06 10:48:25 +0100

    feat: add init command

--- cli.ts
+++ cli.ts
@@ -2,11 +2,36 @@ import { startServer } from "./mod.ts";
 import { parseArgs } from "@std/cli/parse-args";
 
 const args = parseArgs(Deno.args, {
-  boolean: ["cached-only"]
+  boolean: ["cached-only"],
+  string: ["port"],
 });
 
-const server = startServer({
-  useMemCache: true,
-  cachedOnly: args["cached-only"],
-});
-console.error(`Server listening on http://localhost:${server.addr.port}`);
+const subcommand = args._[0];
+const port = args.port == null ? undefined : parseInt(args.port, 10);
+
+if (subcommand === "init") {
+  await init(port ?? 8000);
+} else {
+  const server = startServer({
+    useMemCache: true,
+    cachedOnly: args["cached-only"],
+    port,
+  });
+  console.error(`Server listening on http://localhost:${server.addr.port}`);
+}
+
+async function init(port: number) {
+  const baseUrl = `http://localhost:${port}`;
+  await writeIfNotExists(".npmrc", `registry=${baseUrl}/npm/\n`);
+  await writeIfNotExists(".env", `JSR_URL=${baseUrl}/jsr/\n`);
+}
+
+async function writeIfNotExists(path: string, content: string) {
+  try {
+    await Deno.lstat(path);
+    console.log(`Skipping ${path} (already exists)`);
+  } catch {
+    await Deno.writeTextFile(path, content);
+    console.log(`Created ${path}`);
+  }
+}
